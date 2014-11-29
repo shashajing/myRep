133150f4046c3adba41b7ee490647a952b8fea23
yanghanjing <115877264@qq.com>
2014-11-29 14:27:19 +0800

    项目初次提交

---
+++ benison/src/main/java/com/benison/App.java
@@ -0,0 +1,13 @@
+package com.benison;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
