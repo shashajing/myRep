961dc8984a0dfcc3b9cadf83bc837d4f73061b01
yanghanjing <115877264@qq.com>
2015-06-04 18:09:58 +0800

    redis

--- benison/src/main/java/com/shashajing/benison/web/Test.java
+++ benison/src/main/java/com/shashajing/benison/web/Test.java
@@ -1,30 +1,19 @@
 package com.shashajing.benison.web;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FilterReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PushbackReader;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.cxf.databinding.DataReader;
-
-import com.sun.corba.se.impl.ior.ByteBuffer;
+import redis.clients.jedis.Jedis;
 
 public class Test extends HttpServlet {
 
@@ -73,6 +62,7 @@ public class Test extends HttpServlet {
     }
     
     public String nameLocal;
+    public String firstName;
     
     public String getName(){
     	return nameLocal;
@@ -80,24 +70,94 @@ public class Test extends HttpServlet {
     public void setName(String name){
     	nameLocal = name;
     }
+    public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+    public int hashCode(){
+    	return firstName.hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object object){
+    	Test bb = (Test)object;
+    	return firstName.equals(bb.getFirstName());
+    }
+    
+    @Override
+    public String toString(){
+    	return firstName+nameLocal;
+    }
+    
+    
+    static class ThreadTest extends Thread {
+    	private String bString;
+    	public List<String> bList;
+    	public ThreadTest(String aString){
+    		bString = aString; 
+    	}
+    	public ThreadTest(List<String> aList){
+    		bList = aList; 
+    	}
+    	
+    	@Override
+    	public void run() {
+    		System.out.println(",,,,,,");
+		}
+    	
+    }
+    
+    static class TestCallable implements Callable{
+
+    	public String aString = "436";
+    	private int dss = 88;
+    	public List<String> bList;
+		@Override
+		public String call() throws Exception {
+			System.out.println(bList.add("7"));
+			return aString;
+		}
+		public TestCallable(String bString){
+			aString = bString;
+		}
+		public TestCallable(){
+		}
+		
+		public void testprivate(){
+			
+			System.out.println("dsgdsg");
+		}
+    }
+    
+    
+    
+    public static Integer aObject = new Integer(1);
+    private static Integer bObject = new Integer(2);
+    private static ReentrantLock lock = new ReentrantLock();
+    private static ReentrantLock locka = new ReentrantLock();
+    private static ReentrantReadWriteLock rlock = new ReentrantReadWriteLock();
+    private static ReentrantReadWriteLock.WriteLock writeLock = rlock.writeLock();
+    private static ReentrantReadWriteLock.ReadLock readLock = rlock.readLock();
+    private static Condition condition = lock.newCondition();
+    private static Condition conditiona = locka.newCondition();
     
     public static void main(String[] aStrings){
     	try {
-           /* Jedis jedis = new Jedis("121.41.41.5");
+            Jedis jedis = new Jedis("120.24.57.200");
             long a = new Date().getTime();
             
             for (int i = 0; i < 1000; i++) {
             	jedis.set("money", "wen"+i);
 			}
-            System.out.println((new Date().getTime() - a)/1000);*/
+            System.out.println((new Date().getTime() - a)/1000);
     		
 //    		FileInputStream inputStream = new FileInputStream("D:\\csw.log");
 //    		OutputStream out = new FileOutputStream("D:\\asw.log");
-    		
-    		System.out.println((14.26-10.186)*5500 + 56000);
-    		
-    		
-    		
     		
        } catch (Exception e) {
     	   e.printStackTrace();
@@ -105,5 +165,13 @@ public class Test extends HttpServlet {
        finally{
        }
    }
-    	
+    
+   
+    
+
+    
 }
+
+
+
+
